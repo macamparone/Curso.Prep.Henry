373fe34521b73d46dd6409c10b316d94884b9c53
Marco Camparone <marco43@hotmail.com>
2020-11-11 10:29:59 -0300

    ¨Finish¨

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,29 +3,41 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
-function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
-  // Tu código:
-}
+  function devolverUltimoElemento(array) {
+    // Devuelve el último elemento de un array
+    // Tu código:
+    return array [array.length -1];
+  }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
-  // Tu código:
+  // Tu código: 
+  return array.length;
 }
 
-function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
-}
+  function incrementarPorUno(array)
+   {
+    // "array" debe ser una matriz de enteros (int/integers)
+    // Aumenta cada entero por 1
+    // y devuelve el array
+    var miarray = [];
+
+    for(var i = 0; i < array.length; i++)
+    {
+      miarray[i] = array[i] + 1;
+      }
+      return miarray;
+   }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
-  // Tu código:
+  // Tu código: 
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,38 +45,80 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
-function dePalabrasAFrase(palabras) {
-  // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
-  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
-}
+  function dePalabrasAFrase(palabras) {
+    // "palabras" es un array de strings/cadenas
+    // Devuelve un string donde todas las palabras estén concatenadas
+    // con espacios entre cada palabra
+    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+    // Tu código:
+    var mispalabras = [];
 
-function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
-  // Tu código:
-}
+  for (var i=0; i< palabras.length; i++)
+  {
+      mispalabras += palabras[i] + " ";
+  }
+    return mispalabras.trim();
+  }
 
+
+  function arrayContiene(array, elemento) {
+    // Comprueba si el elemento existe dentro de "array"
+    // Devuelve "true" si está, o "false" si no está
+    // Tu código:
+
+   for (var i = 0; i < array.length; i++)
+    {
+      if (array[i] === elemento)
+        {
+          return true;
+        }
+    }  
+      return false;
+  }
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-}
+  var total = 0;
+
+  for(var i = 0; i < numeros.length; i++)
+    {
+        total = total + numeros[i] ;
+    }
+  return total;
+ }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var total = 0;
+
+  for (var i = 0; i < resultadosTest.length; i++)
+    {
+      total += resultadosTest[i];
+    }
+  var promedio = total/resultadosTest.length;
+  return promedio;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var maximo = 0;
+  for (var i = 0; i < numeros.length; i++)
+    {
+      if (numeros[i] > maximo)
+      {
+        maximo = numeros[i];
+      } 
+    } 
+  return maximo;
 }
 
 function multiplicarArgumentos() {
@@ -72,6 +126,18 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  
+  if(arguments.length < 1)
+    {
+    return 0;
+    }
+  var argumentostotales = 1;
+
+    for (var i = 0; i < arguments.length; i++)
+    {
+     argumentostotales = a.rgumentostotales * arguments[i];
+    }
+  return argumentostotales; 
 }
 
 // No modificar nada debajo de esta línea
